ac369ca7dbe67dc7bc82f900d0686a7b99000f51
osamingo <osamingo@gmail.com>
2014-10-10 20:48:59 +0900

    Add GetCustomMe function

--- config.go
+++ config.go
@@ -9,20 +9,21 @@ type ParseConfig struct {
 	Url           string
 	ApplicationId string
 	RESTAPIKey    string
+	MasterKey     string
 	TimeOut       time.Duration
 }
 
-func checkConfig(config ParseConfig) error {
-	if config.Url == "" {
+func checkConfig(pc ParseConfig) error {
+	if pc.Url == "" {
 		return errors.New("parse url is empty")
 	}
-	if config.ApplicationId == "" {
+	if pc.ApplicationId == "" {
 		return errors.New("parse application id is empty")
 	}
-	if config.RESTAPIKey == "" {
-		return errors.New("parse rest api key is empty")
+	if pc.RESTAPIKey == "" && pc.MasterKey == "" {
+		return errors.New("parse rest api key and master key are empty")
 	}
-	if config.TimeOut <= 0 {
+	if pc.TimeOut <= 0 {
 		return errors.New("parse time out is a negative number or zero")
 	}
 	return nil
--- parse_test.go
+++ parse_test.go
@@ -16,6 +16,30 @@ func TestParse(t *testing.T) {
 			So(err, ShouldNotBeNil)
 		})
 
+		Convey("If session token is empty", func() {
+			c := ParseConfig{"test", "test", "test", "test", 1000}
+			pc, err := New(c)
+			So(err, ShouldBeNil)
+			_, err = pc.GetMe("")
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("If session token is empty", func() {
+			c := ParseConfig{"test", "test", "test", "test", 1000}
+			pc, err := New(c)
+			So(err, ShouldBeNil)
+			err = pc.GetCustomMe("", new(User))
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("If interface{} is empty", func() {
+			c := ParseConfig{"test", "test", "test", "test", 1000}
+			pc, err := New(c)
+			So(err, ShouldBeNil)
+			err = pc.GetCustomMe("test", nil)
+			So(err, ShouldNotBeNil)
+		})
+
 	})
 
 }
